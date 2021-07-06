a58ca81652ba55e324d31ef490da40339eab5f29
Araviel-IO <contact@araviel.io>
2021-07-06 22:08:01 +0200

    control on accounts are ok

--- src/pages/MyWallet/StakePage.js
+++ src/pages/MyWallet/StakePage.js
@@ -5,59 +5,131 @@ import { useEffect } from 'react';
 import Title from '../../components/common/Title';
 import Card from '../../components/common/Card';
 
-import {wCreateStakeAccount , wCreateAuthKeypair} from '../../utils/stake';
+import {wCreateStakeAccount , wCreateAuthKeypair, wCreateStakeKeypair} from '../../utils/stake';
 import { wKeypair} from '../../utils/connection'
 import * as web from '@safecoin/web3.js';
+
 const network = localStorage.getItem('network')
 const connection = new web.Connection(network, "processed");
+
 function StakePage(props) {
 
-    const [authKp, setauthKp] = useState(null);
+    // never use those hooks for sending transactions, except if you know what you
+    // are doing (callbacks)
+    const [accAdd, setaccAdd] = useState(null);
+    const [accbal, setaccbal] = useState(null);
+
+    const [authKp, setauthAdd] = useState(null);
     const [authbal, setauthbal] = useState(null);
+
+    const [stakeAdd, setstakeAdd] = useState(null);
+    const [stakebal, setstakebal] = useState(null);
+
     async function getAuthKeypair() {
 
-        var isSaved = localStorage.getItem('auth-mnemonic')
-        if (isSaved == null || isSaved === undefined) {
-            wCreateAuthKeypair();
-        } 
-        var mnauth = localStorage.getItem('auth-mnemonic')
-        var authkeypair = await wKeypair(mnauth); // don't forget to right click on wKeypair > Go to definition for more
+        var mnMain = localStorage.getItem('mnemonic')
+        var mnAuth = localStorage.getItem('auth-mnemonic')
+        var mnStake = localStorage.getItem('stake-mnemonic')
+        if (mnAuth == null || mnAuth === undefined) {wCreateAuthKeypair();} 
+        if (mnStake == null || mnStake === undefined) {wCreateStakeKeypair();} 
+
+        //var mnauth = localStorage.getItem('auth-mnemonic')
+        var mainkeypair = await wKeypair(mnMain);
+        var authkeypair = await wKeypair(mnAuth); // don't forget to right click on wKeypair > Go to definition for more
+        var stakekeypair = await wKeypair(mnStake);
           //  getAuthkpPromiseEffect(authkeypair.publicKey.toBase58())
-        var  authpeubkey = authkeypair.publicKey.toBase58();
-        
-        setauthKp(authpeubkey);
-        var getbalance = await connection.getBalance(authkeypair.publicKey);
+        var mainAddress = mainkeypair.publicKey.toBase58();
+        var authAddress = authkeypair.publicKey.toBase58();
+        var stakeAddress = stakekeypair.publicKey.toBase58();
+
+        setaccAdd(mainAddress);
+        setauthAdd(authAddress);
+        setstakeAdd(stakeAddress);
+/*
+// TODO: replace getbalance by getaccountifo and hook data
+        await connection.getAccountInfo(mainkeypair.publicKey).then(function(result){ 
+            console.log("getAccountInfo mainkeypair : ", result); 
+           // setaccbal(result);
+        });
+*/
+
+        await connection.getBalance(mainkeypair.publicKey).then(function(result){ 
+           // console.log("requestAirdrop: "+ result); 
+            setaccbal(result);
+        });
+
+        await connection.getBalance(authkeypair.publicKey).then(function(result){ 
+           // console.log("requestAirdrop: "+ result); 
+            setauthbal(result);
+        });
+        await connection.getBalance(stakekeypair.publicKey).then(function(result){ 
+           // console.log("requestAirdrop: "+ result); 
+            setstakebal(result);
+        });
         
         await connection.requestAirdrop(authkeypair.publicKey, 2 * web.LAMPORTS_PER_SAFE)
         .then( 
-            function(TransactionSignature)
+            function(airdropResult)
             { 
-                console.log("requestAirdrop: "+ TransactionSignature); 
-               setauthbal(getbalance);
-               // return TransactionSignature;
+                console.log("requestAirdrop: ", airdropResult); 
             })
         .catch((e)=>{console.log("error",e)});
-
-        
-        console.log("Auth keypair : ", authkeypair);
-        console.log("authpeubkey : ", authpeubkey);
-        console.log("isSaved : ", isSaved);
-
     }
+
     useEffect(() => {
         getAuthKeypair();
+       // getMainAccountKeypair();
 
     });
+
+    // never returns actions if accounts are not loaded
+    function displayCreateControl() {
+        var AuthSave = localStorage.getItem('auth-mnemonic')
+        var MainSave = localStorage.getItem('mnemonic')
+        var StakeSave = localStorage.getItem('stake-mnemonic')
+        if (AuthSave != null && MainSave != null && StakeSave != null)
+        {
+
+           // var accountkeypair = await wKeypair(isMainSaved); 
+            //var authkeypair = await wKeypair(isAuthSaved); 
+
+            return (  
+                <div className="card-button-center" onClick={() => {wCreateStakeAccount(MainSave, AuthSave, StakeSave)} }>Initialize</div>
+            )
+        }
+
+    }
 
     return (
         <div>
             <Title titleHeader='Stake'/>
             <Card cardContent={
                 <div>
-                    <div>Authority address :</div>
-                    <div>{authKp}</div>
-                    <div>{authbal}</div>
-                    <button onClick={wCreateStakeAccount}>Create & delegate</button>
+                    <div className="card-subtitle">Main address :</div>
+                    <div className="receive-address">{accAdd}</div>
+                    <div className="stake-account-infos">
+                        <div>Balance : </div>
+                        <div><b>{accbal / web.LAMPORTS_PER_SAFE}</b></div>
+                        <div className="account-owner">Owner :</div>
+                    </div>
+                    
+                    <div className="card-subtitle">Authority address :</div>
+                    <div className="receive-address">{authKp}</div>
+                    <div className="stake-account-infos">
+                        <div>Balance : </div>
+                        <div><b>{authbal / web.LAMPORTS_PER_SAFE}</b></div>
+                        <div className="account-owner">Owner :</div>
+                    </div>
+                    
+                    <div className="card-subtitle">Stake address :</div>
+                    <div className="receive-address">{stakeAdd}</div>
+                    <div className="stake-account-infos">
+                        <div>Balance : </div>
+                        <div><b>{stakebal / web.LAMPORTS_PER_SAFE}</b></div>
+                        <div className="account-owner">Owner :</div>
+                    </div>
+                    
+                  { displayCreateControl()}
                 </div>
             }>
             </Card>
--- src/utils/stake.js
+++ src/utils/stake.js
@@ -1,45 +1,40 @@
 // reference : https://github.com/solana-labs/solana-web3.js/blob/982dd0c9efe8b48e26f2dc96a09abe7975b16911/test/stake-program.test.js#L25
 import * as web from '@safecoin/web3.js';
-import nacl from 'tweetnacl';
-import * as bip39 from 'bip39';
-import { derivePath } from 'ed25519-hd-key';
 import { Authorized, Lockup } from '@safecoin/web3.js';
-import { genMnemonic } from './connection';
+import { genMnemonic, wKeypair } from './connection';
 // create authorized keypair from mnemonic
 export function wCreateAuthKeypair() {
- 
+// TODO: Double check
     var authmnemonic = genMnemonic();
     localStorage.setItem('auth-mnemonic', authmnemonic);
-    console.log("nothing saved, creating new auth keypair")
-  
-  
+}
+export function wCreateStakeKeypair() {
+  var stakemnemonic = genMnemonic();
+  localStorage.setItem('stake-mnemonic', stakemnemonic);
 }
 // request airdrops on main account + stake account after creating them
-export async function wCreateStakeAccount() {
+export async function wCreateStakeAccount(mnfrom, mnauthorized, stakeacc) {
+  // every keypairs are created from mnemonics
     const getNetwork = localStorage.getItem('network')
    // wCreateAuthKeypair();
     const connection = new web.Connection(getNetwork, 'recent');
-    const voteAccounts = await connection.getVoteAccounts();
-    const voteAccount = voteAccounts.current.concat(voteAccounts.delinquent)[0];
-    const votePubkey = new web.PublicKey(voteAccount.votePubkey);
-  
-    const from = new web.Account(); // always output keypair object "main" account
-    const authorized = new web.Account(); // always output keypair object
-    console.log("Requesting airdrops... ")
-    console.log("authorized :  ",authorized)
-    await connection.requestAirdrop(from.publicKey, 2 * web.LAMPORTS_PER_SAFE);
-    await connection.requestAirdrop(authorized.publicKey, 2 * web.LAMPORTS_PER_SAFE);
+
+    const from = await wKeypair(mnfrom); // always output keypair object "main" account
+    const authorized = await wKeypair(mnauthorized); // always output keypair object
+    const newStakeAccount = await wKeypair(stakeacc);
+   // console.log("Requesting airdrops... ")
+    //console.log("authorized :  ",authorized)
+    //await connection.requestAirdrop(from.publicKey, 2 * web.LAMPORTS_PER_SAFE);
+    //await connection.requestAirdrop(authorized.publicKey, 2 * web.LAMPORTS_PER_SAFE);
   
     const minimumAmount = await connection.getMinimumBalanceForRentExemption(
       web.StakeProgram.space,
       'recent',
     );
   
-
-
     console.log("Creating new stake account... ")
      // Create Stake account without seed
-     const newStakeAccount = new web.Account();
+     //const newStakeAccount = new web.Account();
      let createAndInitialize = web.StakeProgram.createAccount({
        fromPubkey: from.publicKey,
        stakePubkey: newStakeAccount.publicKey,
@@ -47,7 +42,7 @@ export async function wCreateStakeAccount() {
        lockup: new Lockup(0, 0, new web.PublicKey(0)),
        lamports: minimumAmount + 42,
      });
-     console.log("Prepare tx... ")
+     console.log("*Prepare tx... ")
      await web.sendAndConfirmTransaction(
        connection,
        createAndInitialize,
@@ -57,39 +52,42 @@ export async function wCreateStakeAccount() {
         function(output)
         { 
            // console.log("tx-id: "+TransactionSignature); 
-            console.log("createAndInitialize promise : ", output)
-        });
+            console.log("**createAndInitialize promise : ", output)
+        })
+        .catch((e)=>{
+          console.log("**createAndInitialize promise ERROR",e)
+         // constatus = false;
+      });
     
      var getbalance = await connection.getBalance(newStakeAccount.publicKey);
-     console.log("newStakeAccount * getbalance ", getbalance)
-     console.log("Address :  ", newStakeAccount.publicKey.toBase58())
-
-
-
-
-
-
-     let delegation = web.StakeProgram.delegate({
-       stakePubkey: newStakeAccount.publicKey,
-       authorizedPubkey: authorized.publicKey,
-       votePubkey,
-     })
-     // delegation can take some time and exceed the 30s
-     console.log("Starting delegation TX... ")
-     await web.sendAndConfirmTransaction(connection, delegation, [authorized], {
-       commitment: 'single',
-       skipPreflight: true,
-     })
-     .then( 
-        function(output)
-        { 
-           // console.log("tx-id: "+TransactionSignature); 
-            console.log("delegation : ", output)
-        })
+     console.log("**newStakeAccount * getbalance ", getbalance)
+     console.log("**Address :  ", newStakeAccount.publicKey.toBase58())
 
 }
 
 export async function wDelegate() {
+/*
+  const voteAccounts = await connection.getVoteAccounts();
+  const voteAccount = voteAccounts.current.concat(voteAccounts.delinquent)[0];
+  const votePubkey = new web.PublicKey(voteAccount.votePubkey);
 
 
+  let delegation = web.StakeProgram.delegate({
+    stakePubkey: newStakeAccount.publicKey,
+    authorizedPubkey: authorized.publicKey,
+    votePubkey,
+  })
+  // delegation can take some time and exceed the 30s
+  console.log("Starting delegation TX... ")
+  await web.sendAndConfirmTransaction(connection, delegation, [authorized], {
+    commitment: 'single',
+    skipPreflight: true,
+  })
+  .then( 
+     function(output)
+     { 
+        // console.log("tx-id: "+TransactionSignature); 
+         console.log("delegation : ", output)
+     })
+*/
 }
