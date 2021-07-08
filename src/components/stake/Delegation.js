61613121327971c78d955553624f9b2fad074348
Araviel-IO <contact@araviel.io>
2021-07-08 22:55:30 +0200

    Update Delegation.js

--- src/components/stake/Delegation.js
+++ src/components/stake/Delegation.js
@@ -4,7 +4,13 @@ import React from 'react';
 
 function Delegation(props) {
 // if initialized > show this component
-
+/// if not delegated display :
+/// + validator selection list
+/// + summary of the staking instruction
+/// + approx time of activation duration (epoch)
+/// if delegated display :
+/// + show amount & APY + rewards
+/// actual validator
     return (
         <Card styleName='staking' cardContent={
             <div>
